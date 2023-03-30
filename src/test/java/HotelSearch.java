c5086920f9631a13fae7fa9d4fa83d05d8c9173c
Fryzulek <michal.fryzowicz@poczta.onet.pl>
2023-03-30 10:47:18 +0200

    Dodanie daty wyjazdu i przyjazdu.

--- src/test/java/HotelSearch.java
+++ src/test/java/HotelSearch.java
@@ -15,7 +15,8 @@ public class HotelSearch{
         driver.findElement(By.xpath("//span[text()= 'Search by Hotel or City Name']")).click();
         driver.findElement(By.xpath("//div[@id='select2-drop']//input")).sendKeys("Dubai");
         driver.findElement(By.xpath("//span[@class='select2-match' and text()='Dubai']" )).click();
-
+        driver.findElement(By.name("checkin")).sendKeys("01/04/2023");
+        driver.findElement(By.name("checkout")).sendKeys("04/04/2023");
         
 
         //driver.quit();
