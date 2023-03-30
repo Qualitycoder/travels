0f9d6e615a442ab9e33c5130de1d5d73966ec6e4
Fryzulek <michal.fryzowicz@poczta.onet.pl>
2023-03-30 13:18:36 +0200

    Zmiana daty i miesiąca wakacji oraz naciśniecie przycisku search.

--- src/test/java/HotelSearch.java
+++ src/test/java/HotelSearch.java
@@ -15,11 +15,33 @@ public class HotelSearch{
         driver.findElement(By.xpath("//span[text()= 'Search by Hotel or City Name']")).click();
         driver.findElement(By.xpath("//div[@id='select2-drop']//input")).sendKeys("Dubai");
         driver.findElement(By.xpath("//span[@class='select2-match' and text()='Dubai']" )).click();
-        driver.findElement(By.name("checkin")).sendKeys("01/04/2023");
-        driver.findElement(By.name("checkout")).sendKeys("04/04/2023");
-        
+        driver.findElement(By.name("checkin")).click();
 
-        //driver.quit();
+        driver.findElements(By.xpath("//th[@class='next']"))
+                .stream()
+                .filter(el -> el.isDisplayed())
+                .findFirst()
+                .ifPresent(el -> el.click());
+
+        driver.findElements(By.xpath("//td[@class='day ' and text()= '1']"))
+                .stream()
+                .filter(el -> el.isDisplayed())
+                .findFirst()
+                .ifPresent(el -> el.click());
+
+        driver.findElements(By.xpath("//td[@class='day ' and text()= '6']"))
+                .stream()
+                .filter(el -> el.isDisplayed())
+                .findFirst()
+                .ifPresent(el -> el.click());
+
+        driver.findElement(By.xpath("//button[@class='btn btn-lg btn-block btn-primary pfb0 loader']")).click();
+
+
+        driver.quit();
+
+
+
 
 
 
