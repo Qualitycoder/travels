d969836a1ea97b8b7dc329449bc5d17f1c39b91c
Fryzulek <michal.fryzowicz@poczta.onet.pl>
2023-04-05 09:26:00 +0200

    Optymalizacja kodu.

--- src/test/java/PHPTravels/SignUpTest.java
+++ src/test/java/PHPTravels/SignUpTest.java
@@ -2,10 +2,8 @@ package PHPTravels;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
 
 import java.util.concurrent.TimeUnit;
 
